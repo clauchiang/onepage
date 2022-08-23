7ce2a84e4e57badf7124ca255d7f0f36cceb47ac
C.C <autumnoct1111@gmail.com>
2022-08-23 20:44:43 +0800

    修改分頁

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     const adoptCard = $("#adoptCard");
     const detail = $("#detail");
 
-    axios.get('https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL')
+    axios.get('https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=1000&$skip=0&animal_kind=%e8%b2%93')
         .then((res) => {
 
             let data = res.data;
@@ -23,7 +23,7 @@ $(document).ready(function () {
                     item.animal_sex = "公&nbsp;<i class='fa-solid fa-mars'></i>";
                 }
                 
-                if (item.animal_kind == "貓" && index < 1000)
+                if (index < 100)
                     if (item.album_file !== "") {
                         dataText +=
                             `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
@@ -32,7 +32,7 @@ $(document).ready(function () {
                                         <img src="${item.album_file}" alt=" &nbsp; :(&nbsp;圖片網址失效" class="card-img-top photo" title="編號-${item.animal_subid}">
 
                                         <div class="card-body">
-                                        <h5 class="card-title">no.&nbsp;${index}</h5>
+                                        <h5 class="card-title">no.&nbsp;${index+1}</h5>
                                         <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
                                         <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
                                         </div>
@@ -114,7 +114,7 @@ $(document).ready(function () {
                 data.forEach(petFun);
 
                 function petFun(item, index) {
-                    if (item.animal_kind == "貓" && index < 1000)
+                    if (index < 100)
                         if (item.album_file !== "") {
                             dataText +=
                                 `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
@@ -123,7 +123,7 @@ $(document).ready(function () {
                                         <img src="${item.album_file}" alt=" &nbsp; :( 圖片網址失效" class="card-img-top photo" title="編號-${item.animal_subid}">
 
                                         <div class="card-body">
-                                        <h5 class="card-title">no.&nbsp;${index}</h5>
+                                        <h5 class="card-title">no.&nbsp;${index+1}</h5>
                                         <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
                                         <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
                                         </div>
@@ -206,7 +206,7 @@ $(document).ready(function () {
                 data.forEach(petFun);
 
                 function petFun(item, index) {
-                    if (item.animal_kind == "貓" && index < 2000 && index > 1000)
+                    if (index < 200 && index >= 100)
                         if (item.album_file !== "") {
                             dataText +=
                                 `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
@@ -215,7 +215,7 @@ $(document).ready(function () {
                                         <img src="${item.album_file}" alt=" &nbsp; :( 圖片網址失效" class="card-img-top photo" title="編號-${item.animal_subid}">
 
                                         <div class="card-body">
-                                        <h5 class="card-title">no.&nbsp;${index}</h5>
+                                        <h5 class="card-title">no.&nbsp;${index+1}</h5>
                                         <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
                                         <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
                                         </div>
@@ -298,7 +298,7 @@ $(document).ready(function () {
                 data.forEach(petFun);
 
                 function petFun(item, index) {
-                    if (item.animal_kind == "貓" && index < 3000 && index > 2000)
+                    if (index < 300 && index >= 200)
                         if (item.album_file !== "") {
                             dataText +=
                                 `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
@@ -307,7 +307,7 @@ $(document).ready(function () {
                                         <img src="${item.album_file}" alt=" &nbsp; :( 圖片網址失效" class="card-img-top photo" title="編號-${item.animal_subid}">
 
                                         <div class="card-body">
-                                        <h5 class="card-title">no.&nbsp;${index}</h5>
+                                        <h5 class="card-title">no.&nbsp;${index+1}</h5>
                                         <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
                                         <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
                                         </div>
@@ -390,7 +390,7 @@ $(document).ready(function () {
                 data.forEach(petFun);
 
                 function petFun(item, index) {
-                    if (item.animal_kind == "貓" && index < 4000 && index > 3000)
+                    if (index < 400 && index >= 300)
                         if (item.album_file !== "") {
                             dataText +=
                                 `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
@@ -399,7 +399,7 @@ $(document).ready(function () {
                                         <img src="${item.album_file}" alt=" &nbsp; :( 圖片網址失效" class="card-img-top photo" title="編號-${item.animal_subid}">
 
                                         <div class="card-body">
-                                        <h5 class="card-title">no.&nbsp;${index}</h5>
+                                        <h5 class="card-title">no.&nbsp;${index+1}</h5>
                                         <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
                                         <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
                                         </div>
@@ -482,7 +482,7 @@ $(document).ready(function () {
                 data.forEach(petFun);
 
                 function petFun(item, index) {
-                    if (item.animal_kind == "貓" && index < 5000 && index > 4000)
+                    if (index < 500 && index >= 400)
                         if (item.album_file !== "") {
                             dataText +=
                                 `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
@@ -491,7 +491,7 @@ $(document).ready(function () {
                                         <img src="${item.album_file}" alt=" &nbsp; :( 圖片網址失效" class="card-img-top photo" title="編號-${item.animal_subid}">
 
                                         <div class="card-body">
-                                        <h5 class="card-title">no.&nbsp;${index}</h5>
+                                        <h5 class="card-title">no.&nbsp;${index+1}</h5>
                                         <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
                                         <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
                                         </div>
@@ -574,7 +574,7 @@ $(document).ready(function () {
                 data.forEach(petFun);
 
                 function petFun(item, index) {
-                    if (item.animal_kind == "貓" && index < 6000 && index > 5000)
+                    if (index < 600 && index >= 500)
                         if (item.album_file !== "") {
                             dataText +=
                                 `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
@@ -583,7 +583,7 @@ $(document).ready(function () {
                                         <img src="${item.album_file}" alt=" &nbsp; :( 圖片網址失效" class="card-img-top photo" title="編號-${item.animal_subid}">
 
                                         <div class="card-body">
-                                        <h5 class="card-title">no.&nbsp;${index}</h5>
+                                        <h5 class="card-title">no.&nbsp;${index+1}</h5>
                                         <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
                                         <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
                                         </div>
@@ -666,7 +666,7 @@ $(document).ready(function () {
                 data.forEach(petFun);
 
                 function petFun(item, index) {
-                    if (item.animal_kind == "貓" && index < 7000 && index > 6000)
+                    if (index < 700 && index >= 600)
                         if (item.album_file !== "") {
                             dataText +=
                                 `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
@@ -675,7 +675,99 @@ $(document).ready(function () {
                                         <img src="${item.album_file}" alt=" &nbsp; :( 圖片網址失效" class="card-img-top photo" title="編號-${item.animal_subid}">
 
                                         <div class="card-body">
+                                        <h5 class="card-title">no.&nbsp;${index+1}</h5>
+                                        <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
+                                        <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
+                                        </div>
+                                    </div>                                            
+                            </div>`;
+
+                            detailText += `
+                                <div class="modal fade" id="detailModal${item.animal_id}" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog  modal-dialog-centered">
+                                        <div class="modal-content" >
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">編號:&nbsp;${item.animal_subid}</h5>
+                                            </div>
+                                            <div class="modal-body px-4" >
+                                                <p>品種:&nbsp;${item.animal_Variety}</p>
+                                                <p>毛色:&nbsp;${item.animal_colour}</p>
+                                                <p>性別:&nbsp;${item.animal_sex}</p>
+                                                <p>認養地點:&nbsp;${item.shelter_name}</p>
+                                                <p>地址:&nbsp;${item.shelter_address}</p>
+                                                <p>電話:&nbsp;${item.shelter_tel}</p>
+                                                <p><a href='${item.album_file}' target='_blank'>點擊看大圖&nbsp;<i class='fa-solid fa-arrow-up-right-from-square'></i></a></p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">關閉</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+
+                        } else {
+
+                            dataText +=
+                                `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
+                                    <div class="card catCard " data-toggle="modal" data-target="#detailModal${item.animal_id}">
+
+                                        <img src="./img/alt.png" alt="photo" class="card-img-top photo" title="編號-${item.animal_subid}">
+
+                                        <div class="card-body">
                                         <h5 class="card-title">no.&nbsp;${index}</h5>
+                                        <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
+                                        <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
+                                        </div>
+                                    </div>                                            
+                            </div>`;
+
+                            detailText += `
+                                <div class="modal fade" id="detailModal${item.animal_id}" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog  modal-dialog-centered">
+                                        <div class="modal-content" >
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">編號:&nbsp;${item.animal_subid}</h5>
+                                            </div>
+                                            <div class="modal-body px-4" >
+                                                <p>品種:&nbsp;${item.animal_Variety}</p>
+                                                <p>毛色:&nbsp;${item.animal_colour}</p>
+                                                <p>性別:&nbsp;${item.animal_sex}</p>
+                                                <p>認養地點:&nbsp;${item.shelter_name}</p>
+                                                <p>地址:&nbsp;${item.shelter_address}</p>
+                                                <p>電話:&nbsp;${item.shelter_tel}</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">關閉</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+                        }
+                }
+
+                adoptCard.append(dataText);
+                detail.append(detailText);
+            })
+
+            $("#cat8").on("click", function () {
+
+                adoptCard.empty();
+                let dataText = "";
+                let detailText = "";
+
+                data.forEach(petFun);
+
+                function petFun(item, index) {
+                    if (index < 800 && index >= 700)
+                        if (item.album_file !== "") {
+                            dataText +=
+                                `<div class="col mb-4 wow animate__animated animate__slideInUp mt-4" >
+                                    <div class="card catCard " data-toggle="modal" data-target="#detailModal${item.animal_id}">
+
+                                        <img src="${item.album_file}" alt=" &nbsp; :( 圖片網址失效" class="card-img-top photo" title="編號-${item.animal_subid}">
+
+                                        <div class="card-body">
+                                        <h5 class="card-title">no.&nbsp;${index+1}</h5>
                                         <p class="card-text">品種:&nbsp;${item.animal_Variety}</p>
                                         <p class="card-text">地區:&nbsp;${item.shelter_address.slice(0,3)}</p>
                                         </div>
